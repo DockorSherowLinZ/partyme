cbaea81403cda71feb218edec84f9c5514263e91
ZinLien <sgps51204@gmail.com>
2022-04-01 19:10:37 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,64 @@
+const city_item = document.querySelectorAll('.city_group > .city_item');
+
+city_items.forEach(city_items => {
+    city_item.addEventListener('mouseover', function (e){
+        city_items.forEach(ii => ii.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+function moveToSelected(element) {
+
+    if (element == "next") {
+        var selected = $(".selected").next();
+    } else if (element == "prev") {
+        var selected = $(".selected").prev();
+    } else {
+        var selected = element;
+    }
+
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
+
+    $(selected).removeClass().addClass("selected");
+
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+    $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37: // left
+            moveToSelected('prev');
+            break;
+
+        case 39: // right
+            moveToSelected('next');
+            break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
+
+$('#carousel div').click(function () {
+    moveToSelected($(this));
+});
+
+$('#prev').click(function () {
+    moveToSelected('prev');
+});
+
+$('#next').click(function () {
+    moveToSelected('next');
+});
